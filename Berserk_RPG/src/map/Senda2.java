63affe9c437ad3b4c9d0d9b08957e01323b3e733
daniellargomarulanda-commits <daniellargomarulanda@gmail.com>
2026-04-18 12:59:37 +0200

    Enemigo, enemigo básico y zonas
    en las zonas lo único que he hecho ha sido poner los array de los enemigos para tener algo

--- Berserk_RPG/src/map/Senda2.java
+++ Berserk_RPG/src/map/Senda2.java
@@ -1,5 +1,17 @@
 package map;
 
+import enemies.Enemigo;
+import enemies.EnemigoBasico;
+
 public class Senda2 {
 
+    // Array fijo con los 2 enemigos básicos de la Senda 2
+    private final Enemigo[] enemigos = {
+        new EnemigoBasico("Troll", 150, 28, 12, 10, "Mazazo"),
+        new EnemigoBasico("Sátiro Corrupto", 180, 32, 15, 10, "Cornada")
+    };
+
+    public Enemigo[] getEnemigos() {
+        return enemigos;
+    }
 }
--- Berserk_RPG/src/map/Senda3.java
+++ Berserk_RPG/src/map/Senda3.java
@@ -1,5 +1,16 @@
 package map;
 
+import enemies.Enemigo;
+import enemies.EnemigoBasico;
+
 public class Senda3 {
 
+    private final Enemigo[] enemigos = {
+        new EnemigoBasico("Poseso", 220, 40, 18, 10, "Zarpazo"),
+        new EnemigoBasico("Apóstol Menor", 260, 48, 22, 10, "Golpe demoníaco")
+    };
+
+    public Enemigo[] getEnemigos() {
+        return enemigos;
+    }
 }
