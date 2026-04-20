7c7d8b5022b897bed81746a6aa323d5676e137d7
pedrorodmar <pedro.rodmar@gmail.com>
2026-04-20 19:21:21 +0100

    Agregación de Menú
    He creado el menú del juego, se vienen cositas :)

--- Berserk_RPG/src/controller/Main.java
+++ Berserk_RPG/src/controller/Main.java
@@ -1,27 +1,16 @@
 package controller;
 
-import characters.Guts;
-import combat.Combate;
-import enemies.Enemigo;
-import game.EstadoJuego;
-import map.Senda1;
+import game.Juego;
+
 
 public class Main {
 
 
     public static void main(String[] args) {
 
-        // 1. Crear personaje (usa el que ya tengas, ej: Guts)
-        Guts jugador = new Guts(); // o tu clase concreta
+    	Juego menu = new Juego();
+        menu.iniciar();
 
-        // 2. Crear estado del juego
-        EstadoJuego estadoJuego = new EstadoJuego();
-
-        // 3. Crear senda
-        Senda1 senda1 = new Senda1(jugador, estadoJuego);
-
-        // 4. Ejecutar senda
-        senda1.iniciarSenda();
     }
 
 }
